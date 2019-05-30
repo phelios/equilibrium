b6bf464e7de0655a92e41c2752e724856b16c8ae
Sufendy <sufendy.sufendy@rea-group.com>
2019-05-30 14:12:23 +1000

    Init repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='Dots',
+      version='0.0',
+      description='Some sort of simulator',
+      author='Sufendy',
+      packages=['ui', 'dots']
+      )
